c19fc271be46dcd40fb443ebc92bea158dddfb2b
Bogdan Drozdowski <>
2022-12-11 22:30:06 +0100

    Version 1.0

--- KDE3/install.sh
+++ KDE3/install.sh
@@ -2,29 +2,15 @@
 #
 # Konqueror context menu entries' installer
 #
-# Copyright (C) 2007 Bogdan 'bogdro' Drozdowski, bogdro (at) users . sourceforge . net
+# Copyright (C) 2007,2010 Bogdan 'bogdro' Drozdowski, bogdandr # op . pl
+#	[EN] http://miniurl.pl/bogdro-soft
 #
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software Foudation:
-# 		Free Software Foundation
-# 		51 Franklin Street, Fifth Floor
-# 		Boston, MA 02110-1301
-# 		USA
+# This file may be used only according to the licence of Konqueror itself.
 #
 
 if ( test -z "$HOME" ); then
 
-	if ( test -d /home/`whoami` ); then
+	if ( test -d "/home/`whoami`" ); then
 
 		HOME=/home/`whoami`
 	else
@@ -35,7 +21,7 @@ fi
 
 if ( test -z "$KDEHOME" ); then
 
-	if ( test -d $HOME/.kde ); then
+	if ( test -d "$HOME/.kde" ); then
 
 		KDEHOME=$HOME/.kde
 	else
@@ -44,39 +30,63 @@ if ( test -z "$KDEHOME" ); then
 	fi
 fi
 
-if ( test -e $HOME/bin && ! test -d $HOME/bin ); then
+DESTPATH="$KDEHOME/share/apps/konqueror/servicemenus"
+
+unalias mkdir >/dev/null 2>&1
+unalias cp    >/dev/null 2>&1
+
+FILES="safeRemoveDir.desktop		\
+	truecrypt-mount.desktop		\
+	truecrypt-umount.desktop	\
+	wipefreespace.desktop		\
+	safeRemove.desktop		\
+	Shred-rec.sh			\
+	TrueCrypt-mount.sh		\
+	openssl-decrypt.desktop		\
+	Openssl-decrypt.sh		\
+	openssl-encrypt.desktop		\
+	"
+
+mkdir -p "$DESTPATH"
+cp -f $FILES "$DESTPATH"
+
+if ( test -e "$HOME/bin" && ! test -d "$HOME/bin" ); then
+
+	unset DESTPATH
+	unset FILES
 
 	echo "$HOME/bin exists, but not a directory. Fix this. Install failed."
 	exit 3
 fi
 
-[[ ! -e $HOME/bin ]] && mkdir $HOME/bin
+[[ ! -e "$HOME/bin" ]] && mkdir "$HOME/bin"
 
-if ( ! test -d $HOME/bin ); then
+if ( ! test -d "$HOME/bin" ); then
+
+	unset DESTPATH
+	unset FILES
 
 	echo "$HOME/bin can't be created. Install failed."
 	exit 4
 fi
 
-DESTPATH=$KDEHOME/share/apps/konqueror/servicemenus
+[[ ! -e "$HOME/bin/Shred-rec.sh"       ]] && ln -s "$DESTPATH/Shred-rec.sh"       "$HOME/bin/Shred-rec.sh"
+[[ ! -e "$HOME/bin/TrueCrypt-mount.sh" ]] && ln -s "$DESTPATH/TrueCrypt-mount.sh" "$HOME/bin/TrueCrypt-mount.sh"
+[[ ! -e "$HOME/bin/Openssl-decrypt.sh" ]] && ln -s "$DESTPATH/Openssl-decrypt.sh" "$HOME/bin/Openssl-decrypt.sh"
 
-unalias mkdir >/dev/null 2>&1
-unalias cp    >/dev/null 2>&1
-
-mkdir -p $DESTPATH
-cp -f desktop/* $DESTPATH
-
-[[ ! -e $HOME/bin/Shred-rec.sh       ]] && ln -s $DESTPATH/Shred-rec.sh       $HOME/bin/Shred-rec.sh
-[[ ! -e $HOME/bin/TrueCrypt-mount.sh ]] && ln -s $DESTPATH/TrueCrypt-mount.sh $HOME/bin/TrueCrypt-mount.sh
-[[ ! -e $HOME/bin/Openssl-decrypt.sh ]] && ln -s $DESTPATH/Openssl-decrypt.sh $HOME/bin/Openssl-decrypt.sh
-
-if (       ! test -e $HOME/bin/Shred-rec.sh		\
-	|| ! test -e $HOME/bin/TrueCrypt-mount.sh	\
-	|| ! test -e $HOME/bin/Openssl-decrypt.sh	\
+if (       ! test -e "$HOME/bin/Shred-rec.sh"		\
+	|| ! test -e "$HOME/bin/TrueCrypt-mount.sh"	\
+	|| ! test -e "$HOME/bin/Openssl-decrypt.sh"	\
    ); then
+
+	unset DESTPATH
+	unset FILES
 
 	echo "Install failed."
 	exit 5
 fi
+
+unset DESTPATH
+unset FILES
 
 echo "Install OK. Restart Konqueror."
--- KDE3/uninstall.sh
+++ KDE3/uninstall.sh
@@ -2,29 +2,15 @@
 #
 # Konqueror context menu entries' uninstaller
 #
-# Copyright (C) 2007 Bogdan 'bogdro' Drozdowski, bogdro (at) users . sourceforge . net
+# Copyright (C) 2007,2010 Bogdan 'bogdro' Drozdowski, bogdandr # op . pl
+#	[EN] http://miniurl.pl/bogdro-soft
 #
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software Foudation:
-# 		Free Software Foundation
-# 		51 Franklin Street, Fifth Floor
-# 		Boston, MA 02110-1301
-# 		USA
+# This file may be used only according to the licence of Konqueror itself.
 #
 
 if ( test -z "$HOME" ); then
 
-	if ( test -d /home/`whoami` ); then
+	if ( test -d "/home/`whoami`" ); then
 
 		HOME=/home/`whoami`
 	else
@@ -35,7 +21,7 @@ fi
 
 if ( test -z "$KDEHOME" ); then
 
-	if ( test -d $HOME/.kde ); then
+	if ( test -d "$HOME/.kde" ); then
 
 		KDEHOME=$HOME/.kde
 	else
@@ -44,7 +30,7 @@ if ( test -z "$KDEHOME" ); then
 	fi
 fi
 
-DESTPATH=$KDEHOME/share/apps/konqueror/servicemenus
+DESTPATH="$KDEHOME/share/apps/konqueror/servicemenus"
 
 unalias rm >/dev/null 2>&1
 
@@ -55,18 +41,21 @@ FILES="safeRemoveDir.desktop		\
 	safeRemove.desktop		\
 	Shred-rec.sh			\
 	TrueCrypt-mount.sh		\
-	wipe.desktop			\
 	openssl-decrypt.desktop		\
 	Openssl-decrypt.sh		\
 	openssl-encrypt.desktop		\
 	"
 
 for i in $FILES; do
-	rm -f $DESTPATH/$i
+	rm -f "$DESTPATH/$i"
 done;
 
-[[ -L $HOME/bin/Shred-rec.sh       ]] && rm -f $HOME/bin/Shred-rec.sh
-[[ -L $HOME/bin/TrueCrypt-mount.sh ]] && rm -f $HOME/bin/TrueCrypt-mount.sh
-[[ -L $HOME/bin/Openssl-decrypt.sh ]] && rm -f $HOME/bin/Openssl-decrypt.sh
+[[ -L "$HOME/bin/Shred-rec.sh"       ]] && rm -f "$HOME/bin/Shred-rec.sh"
+[[ -L "$HOME/bin/TrueCrypt-mount.sh" ]] && rm -f "$HOME/bin/TrueCrypt-mount.sh"
+[[ -L "$HOME/bin/Openssl-decrypt.sh" ]] && rm -f "$HOME/bin/Openssl-decrypt.sh"
+
+unset DESTPATH
+unset i
+unset FILES
 
 echo "Uninstall OK. Restart Konqueror."
