810287ef6d46176f4a217afea04388a3e9a4fc2f
Bogdan Drozdowski <>
2022-12-11 22:30:02 +0100

    Version 0.9

--- KDE3/desktop/Shred-rec.sh
+++ KDE3/desktop/Shred-rec.sh
@@ -2,64 +2,72 @@
 #
 # "Safe remove directory (recurse)" Konqueror context menu entry - program.
 #
-# Copyright (C) 2007 Bogdan 'bogdro' Drozdowski, bogdro (at) users . sourceforge . net
+# Copyright (C) 2007,2009 Bogdan 'bogdro' Drozdowski, bogdandr # op . pl
+#	[EN] http://miniurl.pl/bogdro-soft
 #
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software Foudation:
-# 		Free Software Foundation
-# 		51 Franklin Street, Fifth Floor
-# 		Boston, MA 02110-1301
-# 		USA
+# This file may be used only according to the licence of Konqueror itself.
 #
 
 
-if ( test "x$1" = "x" ); then
+if ( test $# -lt 1 ); then
 
 	exit 1;
 fi;
 
-find $1 -type f -exec shred -u '{}' \;
-
-dirs=`find $1 -type d | nl | sort -r | awk '{print $2}'`
-
 RANDOM=`date +%k%d%M%j%S`
 
-for i in $dirs; do
+IFS=~
+echo "$*" | sed 's/~/\n/g' | while read; do directory=$REPLY
 
-	if ( test "x$i" = "x." ); then
-		continue;
-	fi;
+	if ( test "x$directory" != "x" ); then
 
-	i=`echo $i | sed "s#^\./##"`
+		find "$directory" -type f -exec shred -u '{}' \;
 
-	len=`echo $i | wc -c`
-	for k in `seq 1 25`; do
+#dirs=`find "$directory" -type d | nl | sort -r | awk '{print $2}'`
+#dirs=`find "$directory" -type d | nl | sort -r | sed 's/^\s*//' | tr '\t' ' '	\
+#	| cut -d ' ' -f '2-' --output-delimiter=' '`
+		dirs=`find "$directory" -type d | nl | sort -r | sed 's/^\s*[0-9]*\s*//'`
 
-		newname=
-		for j in `seq 1 $len`; do
+		find "$directory" -type d | nl | sort -r | sed 's/^\s*[0-9]*\s*//' | while read; do
 
-			rnd=$((`printf %d \'A\'` + (k + $RANDOM)%26))
-			newname="$rnd$newname"
-		done;
+			i=$REPLY
 
-		newname=`dirname $i`/$newname
-		newname=`echo $newname | sed 's#^\./##'`
-		mv $i $newname
-		dirs=`echo $dirs | sed "s#\b$i\b#$newname#g"`
-		i=$newname
-		sync;
-	done;
-done;
+			if ( test "x$i" = "x." ); then
+				continue;
+			fi
 
-rm -fr $newname
+			#i=`echo "$i" | sed 's/^\.\///'`
+			i=`echo "$i" | perl -wne 's/^\.\///; print;'`
+
+			len=`echo "$i" | wc -c`
+			newname=
+			for k in `seq 1 25`; do
+
+				newname=
+				for j in `seq 1 $len`; do
+
+					#rnd=$(((`printf %d \'A\'` + $RANDOM)%26))
+					rnd=$(($RANDOM % 26))
+					newname="$rnd$newname"
+				done;
+
+				newname="`dirname "$i"`/$newname"
+				#newname=`echo "$newname" | sed 's#^\./##'`
+				newname=`echo "$newname" | perl -wne 's~^\./~~; print;'`
+
+				mv "$i" "$newname"
+				#dirs=`echo "$dirs" | sed "s#\b$i\b#$newname#g"`
+				dirs=`echo "$dirs" | perl -wne "s~\b$i\b~$newname~g; print;"`
+				i="$newname"
+				sync;
+			done;
+			if ( test "x$newname" != "x" ); then
+
+				rm -fr "$newname"
+			fi
+
+		done;	# one directory's loop
+	fi # test for empty directory
+
+done;	# all-directory loop
 
